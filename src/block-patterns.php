75f307c765babb130e65dfa787ba034685349898
Rocky Kev <kev.rocky@gmail.com>
2021-06-07 22:08:41 -0700

    working pattern blocks

--- src/block-patterns.php
+++ src/block-patterns.php
@@ -6,14 +6,35 @@
 
 function lizzie_theme_register_my_patterns()
 {
+
+    // register category
+    register_block_pattern_category(
+        'lizzie',
+        array( 'label' => __( 'Lizzie', 'lizzie-theme-underscore' ) )
+    );
+
     register_block_pattern(
-        'lizzie-theme-underscore/my-awesome-pattern',
+        'lizzie-theme-underscore/hero-block',
         array(
+            'categories'    => ['lizzie'],
             'title'       => __('Hero Image', 'lizzie-theme-underscore'),
             'description' => _x('A big hero image with a call to action', 'Block pattern description', 'lizzie-theme-underscore'),
             'content'     => file_get_contents(get_template_directory() . "/block-patterns/full-cover.html")
         )
     );
+
+    register_block_pattern(
+        'lizzie-theme-underscore/image-text-button',
+        array(
+            'categories'    => ['lizzie'],
+            'title'       => __('Column - Image/Text/Button', 'lizzie-theme-underscore'),
+            'description' => _x('A three column approach with image, text, and then a button', 'lizzie-theme-underscore'),
+            'content'     => file_get_contents(get_template_directory() . "/block-patterns/columns-image-text-button.html")
+        )
+    );
+
+
+
 }
 
 
